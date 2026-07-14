19a2fed4c296bf113a270dba51e7730dc6407be7
kiran2203 <parimikiran2003@gmail.com>
2026-07-14 19:34:15 +0530

    initial project and dependecies setup

---
+++ src/main/java/com/kiran/xml_processing_engine/XmlProcessingEngineApplication.java
@@ -0,0 +1,13 @@
+package com.kiran.xml_processing_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlProcessingEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlProcessingEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiran/xml_processing_engine/XmlProcessingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiran.xml_processing_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlProcessingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
